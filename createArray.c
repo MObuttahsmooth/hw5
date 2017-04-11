16a57ac42c66cbb53d682b8fb9193f23126f7355
Yash Parekh <yparekh39@gmail.com>
2017-04-11 16:08:04 -0500

    prints to file

--- createArray.c
+++ createArray.c
@@ -22,6 +22,7 @@ int getVector(int vector[]){
 	tempString[stringCounter] = '\0';
 	vector[elementCount] = atoi(tempString);
 	elementCount++;
+	fclose(vectorfp);
 	return elementCount;
 }
 
@@ -52,14 +53,30 @@ int getMatrix(int matrix[]){
 	tempString[stringCounter] = '\0';
 	matrix[elementCount] = atoi(tempString);
 	elementCount++;
+	fclose(fp);
 	return rows;
 }
 
+void printResult(int resultVector[], int vectorLength){
+	char resultVectorString[1000];
+	int resultIndex = 0;
+	FILE *fp = fopen("result.txt", "w");
+	if(fp == NULL) { return; }
+	int i = 0;
+	for(i = 0; i < vectorLength-1; i++){
+		fprintf(fp, "%d ", resultVector[i]);
+	}
+	fprintf(fp, "%d", resultVector[i]);
+	fclose(fp);
+}
+
 int main(){
-	int vector[1000];
-	int *matrix = malloc(1000000*sizeof(int));
-	int columns = getVector(vector);
-	int rows = getMatrix(matrix);
-	printf("%i columns: %i, %i rows: %i\n", columns, vector[3], rows, matrix[35]);
+	// int vector[1000];
+	// int *matrix = malloc(1000000*sizeof(int));
+	// int columns = getVector(vector);
+	// int rows = getMatrix(matrix);
+	// printf("%i columns: %i, %i rows: %i\n", columns, vector[3], rows, matrix[35]);
+	int result[8] = {1, 5, 8, 7, 14, 15, 20043, 24};
+	printResult(result, 8);
 	return 0;
 }
